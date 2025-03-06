aba44ad94c6152aee7b70ca450f3dfdc7f169755
John Walsh <88987709+wjohn564@users.noreply.github.com>
2025-03-06 12:45:17 +0000

    Initial commit

---
+++ src/main/java/com/cs4227/cs4227_assignment2_20245424/Cs4227Assignment220245424Application.java
@@ -0,0 +1,13 @@
+package com.cs4227.cs4227_assignment2_20245424;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs4227Assignment220245424Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs4227Assignment220245424Application.class, args);
+	}
+
+}
